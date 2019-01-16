37e900f398b0e548862d7a5693db5ed57eddfcaa
Otávio Garrido Moraes <otavio.grrd@gmail.com>
2019-01-16 12:13:28 -0200

    primeira build no android studio
    build 33
    versão 2.0.6

--- Ressuscitou/app/build/generated/source/buildConfig/debug/br/org/cn/ressuscitou/BuildConfig.java
+++ Ressuscitou/app/build/generated/source/buildConfig/debug/br/org/cn/ressuscitou/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "br.org.cn.ressuscitou";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 32;
-  public static final String VERSION_NAME = "2.0.5";
+  public static final int VERSION_CODE = 33;
+  public static final String VERSION_NAME = "2.0.6";
 }
