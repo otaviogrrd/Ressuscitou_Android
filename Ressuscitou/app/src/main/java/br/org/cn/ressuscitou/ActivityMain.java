ac3e4e97eefa02aa80ca3a550dc330a3e40a9add
Otávio Garrido Moraes <otavio.grrd@gmail.com>
2020-01-03 14:35:13 -0300

    Ajustes Cantos
    Ajustes Cantos

--- Ressuscitou/app/src/main/java/br/org/cn/ressuscitou/ActivityMain.java
+++ Ressuscitou/app/src/main/java/br/org/cn/ressuscitou/ActivityMain.java
@@ -44,7 +44,7 @@ public class ActivityMain extends Activity {
 		cantosClass = ((CantosClass) getApplicationContext());
 		cantosClass.popular();
 
-		editor.putInt("cantosVersaoAssets", 37);
+		editor.putInt("cantosVersaoAssets", 38);
 		editor.commit();
 
 		if (haveWifiConnection() || haveDataConnection()) {
